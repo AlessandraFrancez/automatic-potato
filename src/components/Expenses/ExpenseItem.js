7ac2495fb61f90602cda3e7ed56246e2a221df87
Alessandra <alessandra.francez@poli.ufrj.br>
2021-10-16 23:11:09 -0300

    Added sa silly click counter using useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,30 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
-  const { date, title, amount } = props;
+  const { date, amount, title } = props;
+  const [counter, setCounter] = useState(0);
+
+  const plusHandler = () => {
+    setCounter(counter + 1);
+  };
+
+  const minusHandler = () => {
+    setCounter(counter - 1);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
-        <div className="expense-item__description">
-          <h2>{title}</h2>
-          <div className="expense-item__price">{amount}</div>
-        </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <p className="expense-item__price">Clicks {counter}</p>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+      <button className="expense-item__price" onClick={plusHandler}>+</button>
+      <button className="expense-item__price" onClick={minusHandler}>-</button>
     </Card>
   );
 }
