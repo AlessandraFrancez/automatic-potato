8755f35cbcb4682c5690326f514f1595f8f1d259
name <email>
2021-10-18 19:43:51 -0300

    Fixed lint files folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,29 +9,33 @@ module.exports = {
     node: true,
     es6: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'standard'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
   rules: {
-    indent: [ERROR, 2, { SwitchCase: 1, VariableDeclarator: 0 }],
-    'linebreak-style': [ERROR, 'unix'],
+    indent: [WARN, 2, { SwitchCase: 1, VariableDeclarator: 0 }],
+    'linebreak-style': [WARN, 'unix'],
     'one-var': OFF,
     'space-before-function-paren': OFF,
-    quotes: [ERROR, 'single'],
-    semi: [ERROR, 'always'],
-    'brace-style': [ERROR, '1tbs'],
-    'array-bracket-spacing': [ERROR, 'never'],
-    camelcase: [ERROR, { properties: 'always' }],
-    'keyword-spacing': [ERROR],
-    'eol-last': [ERROR],
-    'no-trailing-spaces': [ERROR],
+    quotes: [WARN, 'single'],
+    semi: [WARN, 'always'],
+    'brace-style': [WARN, '1tbs'],
+    'array-bracket-spacing': [WARN, 'never'],
+    camelcase: [WARN, { properties: 'always' }],
+    'keyword-spacing': [WARN],
+    'eol-last': [WARN],
+    'no-trailing-spaces': [WARN],
     'no-case-declarations': OFF,
     'no-path-concat': OFF,
     'no-unused-expressions': OFF,
     'no-multi-str': OFF,
-    camelcase: OFF
+    'react/prop-types': 0
   },
   globals: {
     Promise: true,
