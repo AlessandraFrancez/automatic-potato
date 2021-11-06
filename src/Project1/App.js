7d9a7db05cdd9bd1b43077e2409717512ce2638e
Alessandra Francez <alessandra.f.pereira@accenture.com>
2021-11-06 20:37:13 -0300

    Added project 2 and changed folder structure

---
+++ src/Project1/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+function App() {
+  const list = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 200.01,
+      date: new Date(2021, 2, 13),
+    },
+    {
+      id: 'e2',
+      title: 'Mouse',
+      amount: 350.21,
+      date: new Date(2020, 5, 27),
+    },
+    {
+      id: 'e3',
+      title: 'Keyboard',
+      amount: 799.99,
+      date: new Date(2021, 7, 5),
+    },
+  ];
+
+  const [expenseList, setList] = useState(list);
+
+  const addExpenseHandler = (data) => {
+    setList((prevState) => {
+      return [data, ...prevState];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onNewExpense={addExpenseHandler} />
+      <Expenses list={expenseList} />
+    </div>
+  );
+}
+
+export default App;
